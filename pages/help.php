8a09197a918d30f929b5c3127673bea59506d15f
Thomas Skerbis <thomas.skerbis@mac.com>
2018-05-27 21:00:53 +0200

    Update help.php

--- pages/help.php
+++ pages/help.php
@@ -8,8 +8,12 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-$file = rex_file::get(rex_path::addon('cke5','README.md'));
+if (rex_i18n::getLocale()!='de_de'){
+     $file = rex_file::get(rex_path::addon('cke5','README.md'));
+}
+else {
+     $file = rex_file::get(rex_path::addon('cke5','README_de_de.md')); 
+}
 $body = rex_markdown::factory()->parse($file);
 $fragment = new rex_fragment();
 $fragment->setVar('body', $body, false);
