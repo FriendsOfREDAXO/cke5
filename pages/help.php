ce148cf4b045f8c5ca4dd9c7bf600a45bece74fa
Thomas Skerbis <thomas.skerbis@mac.com>
2018-06-14 20:02:41 +0200

    cke5-help-page hinzugefügt

--- pages/help.php
+++ pages/help.php
@@ -18,5 +18,5 @@ $body = rex_markdown::factory()->parse($file);
 $fragment = new rex_fragment();
 $fragment->setVar('body', $body, false);
 $content = '<div class="FoR-help">'.$fragment->parse('core/page/section.php').'</div>';
-echo $content;
+echo '<div class="cke5-help-page">'. $content .'</div>';
 ?>
