4fb2dfdd202e657ebdd63793e086e9f1e9e9dcea
Joachim Dörr <mail@joachim-doerr.com>
2019-10-08 03:55:13 +0200

    add default font size

--- lib/Cke5/Creator/Cke5ProfilesCreator.php
+++ lib/Cke5/Creator/Cke5ProfilesCreator.php
@@ -33,7 +33,7 @@ class Cke5ProfilesCreator
         'highlight' => ['yellowMarker', 'greenMarker', 'pinkMarker', 'blueMarker', 'redPen', 'greenPen'],
         'image_toolbar' => ['|', 'imageTextAlternative', 'full', 'alignLeft', 'alignCenter', 'alignRight'],
         'rexlink' => ['internal', 'media'],
-        'fontsize' => ['tiny', 'small', 'big', 'huge', '8', '9',
+        'fontsize' => ['default', 'tiny', 'small', 'big', 'huge', '8', '9',
             '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
             '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
             '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
