47204fdb51e9dac26cba437d497ca05b84425d93
Joachim Dörr <mail@joachim-doerr.com>
2019-10-08 05:49:16 +0200

    add preview helper and use it in main preview

---
+++ lib/Cke5/Utils/Cke5PreviewHelper.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @author mail[at]doerr-softwaredevelopment[dot]com Joachim Doerr
+ * @package redaxo5
+ * @license MIT
+ */
+
+namespace Cke5\Utils;
+
+
+use rex_addon;
+use rex_i18n;
+
+class Cke5PreviewHelper
+{
+    /**
+     * @param array $profile
+     * @return string
+     * @author Joachim Doerr
+     */
+    public static function getMFormCode(array $profile)
+    {
+        return (rex_addon::exists('mform')) ? '<pre><span style="color: #aa0000">$mform</span>-&gt;<span style="color: #1e90ff">addTextAreaField</span>(<span style="color: #aa5500">&#39;1&#39;</span>, [<span style="color: #aa5500">&#39;class&#39;</span> =&gt; <span style="color: #aa5500">&#39;cke5-editor&#39;</span>, <span style="color: #aa5500">&#39;data-lang&#39;</span> =&gt; \Cke5\Utils\Cke5Lang::<span style="color: #1e90ff">getUserLang</span>(), <span style="color: #aa5500">&#39;data-content-lang&#39;</span> =&gt; \Cke5\Utils\Cke5Lang::<span style="color: #1e90ff">getOutputLang</span>(), <span style="color: #aa5500">&#39;data-profile&#39;</span> =&gt; <span style="color: #aa5500">&#39;default&#39;</span>]);</pre>' : '';
+
+    }
+
+    /**
+     * @param array $profile
+     * @return string
+     * @author Joachim Doerr
+     */
+    public static function getHtmlCode(array $profile)
+    {
+        return '<pre style=\'color:#000020;background:#f6f8ff;\'><span style=\'color:#0057a6; \'>&lt;</span><span style=\'color:#200080; font-weight:bold; \'>textarea</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>class</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"form-control cke5-editor"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>data</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>profile</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"' . $profile['name'] . '"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>data</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>lang</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#333385; background:#cceeee; \'>&lt;?php</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#200080; background:#cceeee; font-weight:bold; \'>echo</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Cke5</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Utils</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#000000; background:#cceeee; \'>Cke5Lang</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#000000; background:#cceeee; \'>getUserLang</span><span style=\'color:#308080; background:#cceeee; \'>(</span><span style=\'color:#308080; background:#cceeee; \'>)</span><span style=\'color:#406080; background:#cceeee; \'>;</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#333385; background:#cceeee; \'>?></span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>data</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>content</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>lang</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#333385; background:#cceeee; \'>&lt;?php</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#200080; background:#cceeee; font-weight:bold; \'>echo</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Cke5</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Utils</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#000000; background:#cceeee; \'>Cke5Lang</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#000000; background:#cceeee; \'>getOutputLang</span><span style=\'color:#308080; background:#cceeee; \'>(</span><span style=\'color:#308080; background:#cceeee; \'>)</span><span style=\'color:#406080; background:#cceeee; \'>;</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#333385; background:#cceeee; \'>?></span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>name</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"REX_INPUT_VALUE[1]"</span><span style=\'color:#0057a6; \'>></span>REX_VALUE[<span style=\'color:#008c00; \'>1</span>]<span style=\'color:#0057a6; \'>&lt;/</span><span style=\'color:#200080; font-weight:bold; \'>textarea</span><span style=\'color:#0057a6; \'>></span></pre>';
+    }
+
+    /**
+     * @param array $profile
+     * @return string
+     * @author Joachim Doerr
+     */
+    public static function getProfileDetails(array $profile)
+    {
+        $result = \Cke5\Creator\Cke5ProfilesCreator::mapProfile($profile);
+        return '<a href="#profile' . $profile['id'] . '" data-toggle="collapse">' . rex_i18n::msg('cke5_editor_preview_shop_profile_settings') . '</a>
+            <div id="profile' . $profile['id'] . '" class="collapse">
+                <pre class="json_id_' . $profile['id'] . '">
+                    ' . print_r(json_encode($result['profile'], JSON_PRETTY_PRINT), true) . '
+                </pre>
+            </div>
+            <script>
+            $(document).ready(function(){
+                $(\'.json_id_' . $profile['id'] . '\').rainbowJSON();
+            });
+            </script>';
+    }
+
+    public static function getProfilePreview(array $profile, $html = true, $mform = true, $details = true)
+    {
+        $code = '';
+
+        if ($html || $mform || $details) {
+            $code = '<div class="cke5-preview-code">';
+            $code .= ($html) ? \Cke5\Utils\Cke5PreviewHelper::getHtmlCode($profile) : '';
+            $code .= ($mform) ? \Cke5\Utils\Cke5PreviewHelper::getMFormCode($profile) : '';
+            $code .= ($details) ? \Cke5\Utils\Cke5PreviewHelper::getProfileDetails($profile) : '';
+            $code .= '</div>';
+        }
+
+        return '        <div class="cke5-preview-row">
+            <div class="cke5-preview-editor">
+                <h4>"<a href="index.php?page=cke5/profiles&func=edit&id=' . $profile['id'] . '">' . $profile['name'] . '</a>" - ' . $profile['description'] . '</h4>
+                <div class="cke5-editor" data-profile="' . $profile['name'] . '"></div>
+                ' . $code . '
+            </div>
+        </div>';
+    }
+}
--- pages/main.preview.php
+++ pages/main.preview.php
@@ -17,42 +17,10 @@ $profiles = $sql->getArray("SELECT * FROM $table");
 $content .= '<div class="cke5-preview">';
 
 foreach ($profiles as $profile) {
-    $mform_preview = (rex_addon::exists('mform')) ? '<pre><span style="color: #aa0000">$mform</span>-&gt;<span style="color: #1e90ff">addTextAreaField</span>(<span style="color: #aa5500">&#39;1&#39;</span>, [<span style="color: #aa5500">&#39;class&#39;</span> =&gt; <span style="color: #aa5500">&#39;cke5-editor&#39;</span>, <span style="color: #aa5500">&#39;data-lang&#39;</span> =&gt; \Cke5\Utils\Cke5Lang::<span style="color: #1e90ff">getUserLang</span>(), <span style="color: #aa5500">&#39;data-profile&#39;</span> =&gt; <span style="color: #aa5500">&#39;default&#39;</span>]);</pre>' : '';
-
-    $result = \Cke5\Creator\Cke5ProfilesCreator::mapProfile($profile);
-
-    $profile_preview = '
-            <a href="#profile' . $profile['id'] . '" data-toggle="collapse">' . rex_i18n::msg('cke5_editor_preview_shop_profile_settings') . '</a>
-            <div id="profile' . $profile['id'] . '" class="collapse">
-                <pre class="json_id_' . $profile['id'] . '">
-                    ' . print_r(json_encode($result['profile'], JSON_PRETTY_PRINT), true) . '
-                </pre>
-            </div>
-            <script>
-            $(document).ready(function(){
-                $(\'.json_id_' . $profile['id'] . '\').rainbowJSON();
-            });
-            </script>
-';
-
-
-    $content .= '
-        <div class="cke5-preview-row">
-            <div class="cke5-preview-editor">
-                <h4>"<a href="index.php?page=cke5/profiles&func=edit&id=' . $profile['id'] . '">' . $profile['name'] . '</a>" - ' . $profile['description'] . '</h4>
-                    <div class="cke5-editor" data-profile="' . $profile['name'] . '"></div>
-                <div class="cke5-preview-code">
-                    <pre style=\'color:#000020;background:#f6f8ff;\'><span style=\'color:#0057a6; \'>&lt;</span><span style=\'color:#200080; font-weight:bold; \'>textarea</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>class</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"form-control cke5-editor"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>data</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>profile</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"' . $profile['name'] . '"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>data</span><span style=\'color:#474796; \'>-</span><span style=\'color:#074726; \'>lang</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#333385; background:#cceeee; \'>&lt;?php</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#200080; background:#cceeee; font-weight:bold; \'>echo</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Cke5</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#0066ee; background:#cceeee; \'>Utils</span><span style=\'color:#406080; background:#cceeee; \'>\</span><span style=\'color:#000000; background:#cceeee; \'>Cke5Lang</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#406080; background:#cceeee; \'>:</span><span style=\'color:#000000; background:#cceeee; \'>getUserLang</span><span style=\'color:#308080; background:#cceeee; \'>(</span><span style=\'color:#308080; background:#cceeee; \'>)</span><span style=\'color:#406080; background:#cceeee; \'>;</span><span style=\'color:#000000; background:#cceeee; \'> </span><span style=\'color:#333385; background:#cceeee; \'>?></span><span style=\'color:#1060b6; \'>"</span><span style=\'color:#474796; \'> </span><span style=\'color:#074726; \'>name</span><span style=\'color:#308080; \'>=</span><span style=\'color:#1060b6; \'>"REX_INPUT_VALUE[1]"</span><span style=\'color:#0057a6; \'>></span>REX_VALUE[<span style=\'color:#008c00; \'>1</span>]<span style=\'color:#0057a6; \'>&lt;/</span><span style=\'color:#200080; font-weight:bold; \'>textarea</span><span style=\'color:#0057a6; \'>></span></pre>
-                    ' . $mform_preview . '
-                    ' . $profile_preview . '
-                </div>
-            </div>
-        </div>
-    ';
+    $content .= \Cke5\Utils\Cke5PreviewHelper::getProfilePreview($profile);
 }
 
 $content .= '</div>';
-
 
 $content = \Cke5\Provider\Cke5NavigationProvider::getSubNavigationHeader() .
     \Cke5\Provider\Cke5NavigationProvider::getSubNavigation() .
