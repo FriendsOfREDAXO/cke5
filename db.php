751efdb6d0721f786a2b1d682dad5b5b0971a50a
Joachim Doerr <mail@joachim-doerr.com>
2020-07-15 17:12:49 +0200

    fix install issue

---
+++ db.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @author mail[at]doerr-softwaredevelopment[dot]com Joachim Doerr
+ * @package redaxo5
+ * @license MIT
+ */
+
+// install profiles database
+$sql = rex_sql_table::get(rex::getTable('cke5_profiles'));
+$sql->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('name', 'varchar(40)', true))
+    ->ensureColumn(new rex_sql_column('description', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('toolbar', 'text', true))
+    ->ensureColumn(new rex_sql_column('expert_definition', 'text', true))
+    ->ensureColumn(new rex_sql_column('expert_suboption', 'text', true))
+    ->ensureColumn(new rex_sql_column('expert', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('extra_definition', 'text', true))
+    ->ensureColumn(new rex_sql_column('extra', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('code_block', 'text', true))
+    ->ensureColumn(new rex_sql_column('special_characters', 'text', true))
+    ->ensureColumn(new rex_sql_column('group_when_full', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('table_color_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('table_color', 'text', true))
+    ->ensureColumn(new rex_sql_column('ytable', 'text', true))
+    ->ensureColumn(new rex_sql_column('transformation', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('transformation_extra', 'text', true))
+    ->ensureColumn(new rex_sql_column('transformation_remove', 'text', true))
+    ->ensureColumn(new rex_sql_column('transformation_include', 'text', true))
+    ->ensureColumn(new rex_sql_column('blank_to_external', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('link_downloadable', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('link_decorators', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('link_decorators_definition', 'text', true))
+    ->ensureColumn(new rex_sql_column('heading', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('alignment', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('image_toolbar', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('fontsize', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('highlight', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('table_toolbar', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('rexlink', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('height_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('min_height', 'int(4)', true))
+    ->ensureColumn(new rex_sql_column('max_height', 'int(4)', true))
+    ->ensureColumn(new rex_sql_column('lang', 'varchar(2)', true))
+    ->ensureColumn(new rex_sql_column('lang_content', 'varchar(2)', true))
+    ->ensureColumn(new rex_sql_column('font_color', 'text', true))
+    ->ensureColumn(new rex_sql_column('font_color_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('font_background_color', 'text', true))
+    ->ensureColumn(new rex_sql_column('font_background_color_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('font_families', 'text', true))
+    ->ensureColumn(new rex_sql_column('font_family_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('mediaembed', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('mediatype', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('mediapath', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('mediacategory', 'int(4)', true))
+    ->ensureColumn(new rex_sql_column('upload_default', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('updatedate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('createuser', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('updateuser', 'varchar(255)', true));
+
+foreach (rex_i18n::getLocales() as $locale) {
+    $sql->ensureColumn(new rex_sql_column('placeholder_' . $locale, 'varchar(255)', true));
+}
+
+$sql->ensure();
+
+// install mblock demo database
+rex_sql_table::get(rex::getTable('cke5_mblock_demo'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('name', 'varchar(40)', true))
+    ->ensureColumn(new rex_sql_column('mblock_field', 'text', true))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('updatedate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('createuser', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('updateuser', 'varchar(255)', true))
+    ->ensure();
--- update.php
+++ update.php
@@ -4,21 +4,11 @@
  * @package redaxo5
  * @license MIT
  */
-use Cke5\Creator\Cke5ProfilesCreator;
-use Cke5\Handler\Cke5DatabaseHandler;
+
+include_once (__DIR__ . '/db.php');
 
 try {
     if (rex_string::versionCompare($this->getVersion(), '3.3.0', '<')) {
-        // add content lang column
-        rex_sql_table::get(rex::getTable('cke5_profiles'))
-            ->ensureColumn(new rex_sql_column('lang_content', 'varchar(2)', true))
-            ->ensureColumn(new rex_sql_column('font_color', 'text', true))
-            ->ensureColumn(new rex_sql_column('font_color_default', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('font_background_color', 'text', true))
-            ->ensureColumn(new rex_sql_column('font_background_color_default', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('font_families', 'text', true))
-            ->ensureColumn(new rex_sql_column('font_family_default', 'varchar(255)', true))
-            ->ensure();
         // copy custom data to assets folder
         if (!file_exists(rex_path::assets('addons/cke5_custom_data'))) {
             mkdir(rex_path::assets('addons/cke5_custom_data'));
@@ -26,31 +16,6 @@ try {
         if (!file_exists(rex_path::assets('addons/cke5_custom_data/custom-style.css'))) {
             rex_file::copy($this->getPath('custom_data/custom-styles.css'), rex_path::assets('addons/cke5_custom_data/custom-style.css'));
         }
-    }
-    if (rex_string::versionCompare($this->getVersion(), '4.0.0', '<')) {
-        // add placeholder lang columns
-        $sql = rex_sql_table::get(rex::getTable('cke5_profiles'));
-        foreach (rex_i18n::getLocales() as $locale) {
-            $sql->ensureColumn(new rex_sql_column('placeholder_' . $locale, 'varchar(255)', true));
-        }
-        $sql->ensureColumn(new rex_sql_column('expert_definition', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('definition', 'text', true))
-            ->ensureColumn(new rex_sql_column('extra_definition', 'text', true))
-            ->ensureColumn(new rex_sql_column('special_characters', 'text', true))
-            ->ensureColumn(new rex_sql_column('code_block', 'text', true))
-            ->ensureColumn(new rex_sql_column('table_color_default', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('table_color', 'text', true))
-            ->ensureColumn(new rex_sql_column('transformation', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('transformation_extra', 'text', true))
-            ->ensureColumn(new rex_sql_column('transformation_remove', 'text', true))
-            ->ensureColumn(new rex_sql_column('transformation_include', 'text', true))
-            ->ensureColumn(new rex_sql_column('blank_to_external', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('link_downloadable', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('link_decorators', 'varchar(255)', true))
-            ->ensureColumn(new rex_sql_column('link_decorators_definition', 'text', true))
-            ->ensureColumn(new rex_sql_column('ytable', 'text', true))
-            ->ensureColumn(new rex_sql_column('group_when_full', 'varchar(255)', true))
-            ->ensure();
     }
 } catch (rex_functional_exception $e) {
     rex_logger::logException($e);
