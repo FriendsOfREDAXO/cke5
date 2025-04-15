b014000b1f1ce681b6f6d67044f0108ebb227aad
Joachim Dörr <git@joachim-doerr.com>
2025-04-15 12:51:41 +0200

    add missing tables to uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,7 @@ $sql = rex_sql::factory();
 
 // Delete tables
 $sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_profiles');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_styles');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_style_groups');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_templates');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_template_groups');
