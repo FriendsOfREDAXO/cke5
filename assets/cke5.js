bb08e5c449fb1d464d21119b7deb67452f1f95ed
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-09 23:14:39 +0200

    Fehlende ImageResizeHandles-Abhängigkeit ergänzen

--- assets/cke5.js
+++ assets/cke5.js
@@ -182,6 +182,7 @@
             "ImageCaption",
             "ImageStyle",
             "ImageResize",
+            "ImageResizeHandles",
             "ImageUpload",
             "AutoImage",
             "MediaEmbed",
