54872f1fe1bb866fe95f7625ee90886b80ef15d8
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-11 02:01:02 +0200

    Fix: Bei Deinstallation snippets-Tabelle mit löschen

--- uninstall.php
+++ uninstall.php
@@ -5,5 +5,4 @@ $sql = rex_sql::factory();
 $sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_profiles');
 $sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_styles');
 $sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_style_groups');
-$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_templates');
-$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_template_groups');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'cke5_snippets');
